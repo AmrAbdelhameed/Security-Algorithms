556d603c067da503dbc111bbede77672b90c7a36
Triplem5ds <mmmanany@yahoo.com>
2019-03-13 14:09:42 +0200

    RailFence Green Light

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -8,19 +8,66 @@ namespace SecurityLibrary
 {
     public class RailFence : ICryptographicTechnique<string, int>
     {
-        public int Analyse(string plainText, string cipherText)
+        private string transform(string str, int key, bool f)
         {
-            throw new NotImplementedException();
+            //if f = 0 plain to cipher meaning upper
+            //else lower
+            List<List<char>> mat = new List<List<char>>();
+
+            for (int i = 0; i < key; i++)
+                mat.Add(new List<char>());
+
+            for (int j = 0, i = 0; i < str.Length; i++, j = (j + 1) % key)
+            {
+                mat[j].Add(str[i]);
+            }
+
+            char[] returnText = new char[str.Length];
+
+            for (int i = 0, ptr = 0; i < key; i++)
+            {
+                for (int j = 0; j < mat[i].Count; j++)
+                {
+                    returnText[ptr++] = mat[i][j];
+                }
+            }
+            if(!f)
+                return new string(returnText).ToUpper();
+            return new string(returnText).ToLower();
+        }
+        public int Analyse(string plainText, string cipherText)
+        {   
+            var builder = new StringBuilder();
+            foreach(char c in plainText)
+                builder.Append(c);
+            while (builder.Length < cipherText.Length)
+                builder.Append('x');
+            plainText = builder.ToString(); 
+            int ans = -1;
+            for (int key = 1; key <= plainText.Length; key++)
+            {
+                if(transform(plainText,key,false) == cipherText)
+                {
+                    ans = key;
+                    break;
+                }
+            }
+            if (ans == -1)
+                throw new InvalidAnlysisException();
+            return ans;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            return transform(cipherText, (cipherText.Length + key - 1) / key, true);
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            string ret = transform(plainText, key,false);
+            Console.WriteLine(ret);
+            Console.WriteLine(plainText);
+            return ret;
         }
     }
 }
