2744d72d1dd3e8ae46947b7e1726597d9a79bee7
Moataz M. El Sakkar <34172775+MoatazElSakkar@users.noreply.github.com>
2019-03-14 05:14:41 +0200

    Repeated Key Vigenère
    -A totally weird method to remove repetetion in analysis.
    -Uncommented Code written at 3:00AM Proced at your own risk
    -Reminder to myself: uncomment tests that raised exceptions
    -M

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -10,17 +10,88 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            Queue<char> Pathfinder = new Queue<char>(); //RepeatedKey Removal
+            Queue<char> PathReserve = new Queue<char>();
+
+            bool streak = false;
+            string nonRep = "";
+            string Key = "";
+            string PT = plainText.ToLower();
+            string CT = cipherText.ToLower();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char Temp = (char)(CT[i] - (PT[i] - 'a'));
+                if (Temp < 'a')
+                {
+                    int index = 26 - (PT[i] - 'a');
+                    Temp = (char)(CT[i] + index);
+                }
+                if (Pathfinder.Count==0 || Temp != Pathfinder.Peek())
+                {
+                    Key += Temp;
+                    Pathfinder.Enqueue(Temp);
+                    nonRep = Key;
+                    if (streak)
+                    {
+                        Pathfinder.Clear();
+                        PathReserve.Clear();
+                        foreach (char c in nonRep)
+                        {
+                            Pathfinder.Enqueue(c);
+                        }
+                        streak = false;
+                    }
+                }
+                else
+                {
+                    Key += Temp;
+                    PathReserve.Enqueue(Pathfinder.Dequeue());
+                    streak = true;
+                }
+                if (Pathfinder.Count==0 && streak)
+                {
+                    while (PathReserve.Count!=0)
+                    {
+                        Pathfinder.Enqueue(PathReserve.Dequeue());
+                    }
+                    streak = false;
+                }
+            }
+            return nonRep;
         }
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            string PT = "";
+            string CT = cipherText.ToLower();
+            for (int i = 0; i < CT.Length; i++)
+            {
+                char Temp = (char)(CT[i] - (key[i % key.Length] - 'a'));
+                if (Temp < 'a')
+                {
+                    int index = 26 - (key[i % key.Length] - 'a');
+                    Temp = (char)(CT[i] + index);
+                }
+                PT += Temp;
+            }
+            return PT;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            string PT = plainText.ToLower();
+            string CT = "";
+            for(int i=0;i<plainText.Length;i++)
+            {
+                char Temp = (char)(plainText[i] + (key[i % key.Length]-'a'));
+                if (Temp > 'z')
+                {
+                    Temp = (char)(Temp % 'z');
+                    Temp += (char)('a'- 1);
+                }
+                CT += Temp;
+            }
+            return CT.ToUpper();
         }
     }
 }
