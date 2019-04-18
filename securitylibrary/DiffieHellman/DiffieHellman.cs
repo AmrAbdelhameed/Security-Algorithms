89059c5be51d90abe37c0585a94ca26cd4f1a27b
AmrAbdelhameed <amrabdelhameedfcis123@gmail.com>
2019-04-18 21:10:03 +0200

    RSA, RC4 and Deffie-Hellman

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -10,7 +10,22 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> result = new List<int>();
+            int ya = pow(alpha, xa, q);
+            int yb = pow(alpha, xb, q);
+            int k1 = pow(yb, xa, q);
+            int k2 = pow(ya, xb, q);
+            result.Add(k1);
+            result.Add(k2);
+            return result;
+        }
+
+        public int pow(int a, int b, int c)
+        {
+            int res = 1;
+            for (int i = 0; i < b; i++)
+                res = (res * a) % c;
+            return res;
         }
     }
 }
